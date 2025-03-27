9fecf5d6086e0698e3c23bca17dbc426ea6b7977
Kalle Fagerberg <kalle.f8+github@proton.me>
2025-03-27 22:05:31 +0100

    Messed up a lot of stuff so far

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,18 @@
-import { join } from 'path';
-import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from "path";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}',
-		),
-		'astro.config.mjs',
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [skeleton({
-		themes: { preset: ['rocket'] },
-	})],
-}
+  darkMode: "class",
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    join(
+      require.resolve("@skeletonlabs/skeleton"),
+      "../**/*.{html,js,svelte,ts}",
+    ),
+    "astro.config.mjs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
